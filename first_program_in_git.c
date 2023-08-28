e3086ddaf3d2a739038af0c7a12791bc9f02d5b3
CodingDev20 <ayoublfdili@gmail.com>
2023-08-28 19:02:47 +0100

    this is simple program in git

---
+++ first_program_in_git.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// multiplication table program in C
+
+
+int main (){
+	int number;
+	pritnf("Enter your number:");
+	scanf("%d",&number);
+	for(int counter = 0 ; counter <=10 ; counter++)
+	{
+	    printf("%d * %d = %d\n",number, conter,number*counter");
+	}
+
+return 0;
+
+
+}
